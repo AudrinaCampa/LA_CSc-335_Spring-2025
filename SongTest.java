31d094b6819f7d0b57151f30e3c5ace3b80fe208
Ignacio Gonzalez <142772066+gonzalezi11@users.noreply.github.com>
2025-02-27 20:33:55 -0700

    Update SongTest.java

--- SongTest.java
+++ SongTest.java
@@ -14,10 +14,51 @@ class SongTest {
 	Album album = new Album("This is How Tomorrow Moves", artist);
 	Song song = new Song(title,artist,album);
 	
-	
-	void testToString() {
+	@Test
+	public void testToString() {
 		assertEquals("Title : Take A Bite, Artist : Beabadoobee, Album : This is How Tomorrow Moves", song.toString());
 		
 	}
-
+	
+	@Test
+	public void testSetRating() {
+		song.setRating(5);
+		int songRating = song.getRating();
+		assertEquals(5,songRating);
+		
+	}
+	
+	@Test
+	public void testSetRatingThrowsException() {
+		try {
+			song.setRating(0); // Should throw an exception
+			
+		} catch (IllegalArgumentException e) {
+			assertEquals("Rating must be in range of 1 and 5", e.getMessage());
+	        
+		}
+		try {
+			song.setRating(6); // Should throw an exception
+	        
+		} catch (IllegalArgumentException e) {
+	            assertEquals("Rating must be in range of 1 and 5", e.getMessage());
+	        }
+	    }
+	
+	@Test
+	public void testGetAlbum() {
+		assertEquals("This is How Tomorrow Moves", song.getAlbum());
+	}
+	
+	@Test
+	public void testGetArtist() {
+		assertEquals("Beabadoobee", song.getArtist());
+	}
+	
+	@Test
+	public void testGetTitle() {
+		assertEquals("Take A Bite", song.getTitle());
+	}
+	
 }
+
