5f1771de76d31a0a52ad4d39671452000f3da12b
Ignacio Gonzalez <142772066+gonzalezi11@users.noreply.github.com>
2025-02-27 17:29:15 -0700

    Update Album.java

--- Album.java
+++ Album.java
@@ -1,5 +1,37 @@
 package model;
 
-public class Album {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Album { 
+	private List<Song> songs;
+	private final String title;
+	private Artist artist;
+	
+	public Album (String title, Artist artist) {
+		this.title = title;
+		this.artist = artist;
+		this.songs = new ArrayList<>();
+	
+	}
+	
+	public Artist getArtists() {
+		return this.artist; 
+	}
+	
+	public String getTitle() {
+		return this.title;
+	}
+	
+	public List<Song> getSongs() {
+		return this.songs;
+		
+	}
+	
+	public void addSong(Song song) {
+		songs.add(song);
+	}
+	
+	
 
 }
