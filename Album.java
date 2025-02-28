d66e79a38be507a2a8390f8b48a75ddb85ad3220
Audrina Campa <146894394+AudrinaCampa@users.noreply.github.com>
2025-02-27 23:29:40 -0700

    Update Album.java

--- Album.java
+++ Album.java
@@ -1,37 +1,25 @@
+/**
+ * 
+ */
 package model;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class Album { 
-	private List<Song> songs;
-	private final String title;
-	private Artist artist;
+/**
+ * 
+ *
+ */
+public class Album {
 	
-	public Album (String title, Artist artist) {
-		this.title = title;
-		this.artist = artist;
-		this.songs = new ArrayList<>();
+	private ArrayList<String> albums;
 	
+	
+	public Album() {
+		this.albums = new ArrayList<String>();
 	}
-	
-	public Artist getArtists() {
-		return this.artist; 
-	}
-	
-	public String getTitle() {
-		return this.title;
-	}
-	
-	public List<Song> getSongs() {
-		return this.songs;
-		
-	}
-	
-	public void addSong(Song song) {
-		songs.add(song);
-	}
-	
-	
 
+
+	public void addAlbum(String title) {
+		this.albums.add(title);
+	}
 }
