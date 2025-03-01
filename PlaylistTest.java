c513486d685be9894c32d617943f2801b29c881e
Ignacio Gonzalez <142772066+gonzalezi11@users.noreply.github.com>
2025-02-28 22:21:40 -0700

    Update PlaylistTest.java

--- PlaylistTest.java
+++ PlaylistTest.java
@@ -2,6 +2,9 @@ package model.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import model.Album;
@@ -10,14 +13,13 @@ import model.PlayList;
 import model.Song;
 
 class PlaylistTest {
+	
 	PlayList playlist = new PlayList("playlist 1");
 	String title = "This is How Tomorrow Moves";
 	Artist artist = new Artist("Beabadoobee");
 	Album album = new Album(title,artist);
 	Song songOne = new Song("Take A Bite", artist, album);
 	Song songTwo = new Song("Ever Seen", artist, album);
-	
-
 
 	@Test
 	public void testGetName() {
@@ -27,20 +29,19 @@ class PlaylistTest {
 	@Test
 	public void testGetSongs() {	
 		playlist.addSong(songTwo);
-		playlist.addSong(songOne);
-		
-		assertEquals("Ever Seen", playlist.getSongs().get(0));
-		assertEquals("Take A Bite", playlist.getSongs().get(1));
-		
+		playlist.addSong(songTwo);
+		assertTrue(playlist.getSongs().contains("Title : " + songTwo.getTitle() + " Artist : " + songTwo.getArtist()));
+
 	}
 	
 	@Test
 	public void testRemoveSong() {
-		playlist.addSong(songTwo);
 		playlist.addSong(songOne);
+		playlist.addSong(songTwo);
 		
-		playlist.removeSong(songTwo);
-		assertEquals("Take A Bite", playlist.getSongs().get(0));
+		playlist.removeSong(songOne);
+		assertTrue(playlist.getSongs().get(0).contains("Title : " + songTwo.getTitle() + " Artist : " + songTwo.getArtist()));
 	}
 
 }
+
