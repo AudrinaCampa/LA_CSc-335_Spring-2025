c35288a27de9adb0d98a0c45f13f7bb7496e9c6f
Audrina Campa <146894394+AudrinaCampa@users.noreply.github.com>
2025-02-28 22:12:19 -0700

    Update LibraryView.java

--- LibraryView.java
+++ LibraryView.java
@@ -1,5 +1,31 @@
 package view;
 
+import java.util.Scanner;
+
+import model.MusicStore;
+import model.PlayList;
+import model.Album;
+import model.LibraryModel;
+import model.Artist;
+import model.Song;
+
+
 public class LibraryView {
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Create Playlist: ");
+		String playlist = input.nextLine();
+		PlayList pl = new PlayList(playlist);
+		
+		System.out.println("Playlist Name: " + playlist);
+		
+		
+		Scanner search = new Scanner(System.in);
+		System.out.print("Search for Song by Artist: ");
+		String artistName = search.nextLine();
+		
 
 }
