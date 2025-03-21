dd4c0a6bf8b67f1cf04d01205cd1515130d33896
Ignacio Gonzalez <142772066+gonzalezi11@users.noreply.github.com>
2025-03-21 11:27:06 -0700

    Update MusicStore.java

--- MusicStore.java
+++ MusicStore.java
@@ -8,101 +8,21 @@ import java.util.List;
 
 public class MusicStore {
     private List<Song> songs;
-    private List<Album> albums;
-    private List<Artist> artists;
-    private String file;
+    private HashMap<Album, List<Song>> albums;
+    private HashMap<Artist, List<Song>> artists;
+    private final String file;
     
     public MusicStore(String filename ) {
     	this.songs = new ArrayList<>();
-    	this.albums = new ArrayList<>();
-    	this.artists = new ArrayList<>();
+    	this.albums = new HashMap<>();
+    	this.artists = new HashMap<>();
     	this.file = filename;
     	readAlbumsList();
     	
     }
-    
-    public void addSong(Song song) {
-    	songs.add(song);
-    }
-    
-    
-    public void addAlbum(Album album) {
-    	albums.add(album);
-    }
-    
-    public void addArtist(Artist artist) {
-    	for (Artist dupliacateArtist : artists) {
-    		if (dupliacateArtist.getName().equals(artist.getName())) {
-    			return;
-    		}
-    	}
-    	artists.add(artist);
-    }
-    
-    public List<String> getArtist() {
-    	List<String> artistNames = new ArrayList<>();
-			for (Artist artist  : artists) {
-				artistNames.add(artist.getName());
-		}
-		return artistNames;
-		
-	}
-    
-    public String searchSongByTitle(String title) {
-    	List<String> results = new ArrayList<>();
-    	for (Song song : songs) {
-    		if (song.getTitle().equals(title)) {
-    			results.add(song.getTitle() + " "+ song.getArtist() + " " + song.getAlbum());
-    		}
-    	}
-    	if (results.isEmpty()) {
-    		return "Song " + title + " not found";
-    	}
-    	
-    	return results.toString();
-    }
-    
-    public String searchSongByArtist(String artist) {
-    	List<String> results = new ArrayList<>();
-    	for (Song song : songs) {
-    		if (song.getArtist().equals(artist)) {
-    			results.add(song.getTitle() + " "+ song.getArtist() + " " + song.getAlbum());
-    		}
-    	}
-    	if (results.isEmpty()) {
-    		return "No songs found for " + artist;
-    	}
-    	return results.toString();    	
-    }
-    
-    public String searchAlbumByTitle(String title) {
-    	List<String> results = new ArrayList<>();
-    	for (Album album : albums) {
-    		if (album.getTitle().equals(title)) {
-    			results.add(album.getTitle() + " " + album.getSongs());
-    		}
-    	}
-    	if (results.isEmpty()) {
-    		return "Album " + title + " not found";
-    	}
-    	
-    	return results.toString();
-    }
-    
-    public String searchAlbumByArtist(String artist) {
-    	List<String> results = new ArrayList<>();
-    	for (Album album : albums) {
-    		if (album.getArtists().equals(artist)) {
-    			results.add(album.getTitle() + " " + album.getSongs());
-    		}
-    	}
-    	if (results.isEmpty()) {
-    		return "No albums found for " + artist;
-    	}
-    	
-    	return results.toString();
-    }
-    
+
+
+
     private void readAlbumsList() {
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
@@ -150,22 +70,6 @@ public class MusicStore {
         } catch (IOException e) {
             System.out.println("Error reading file " + fileName + ": " + e.getMessage());
         }
-    }
-    
-	public List<String> getSongs() {
-		List<String> songList = new ArrayList<>();
-		for (Song song : songs) {
-			songList.add(song.getTitle());
-		}
-		return songList;
-	}
 
-	public List<String> getAlbums() {
-		List<String> albumList = new ArrayList<>();
-		for (Album album : albums) {
-			albumList.add(album.getTitle());
-		}
-		return albumList;
-	}
 	
 }
